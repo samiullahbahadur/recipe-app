773d65a41526134c3847787faff7e0b649fc35c1
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 20:28:44 +0430

    Add all validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,  :confirmable
     has_many :food, dependent: :delete_all
-    has_many :recipe, dependent: :delete_all
+     has_many :recipe, dependent: :delete_all
 
-    validates :name, presence: true
+     validates :name, presence: true
 end
