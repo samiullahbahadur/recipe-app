cd12acf7b9e41d3c3837203561e123a11cec32ee
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 20:20:43 +0430

    Add food migration

--- db/migrate/20220727153338_create_foods.rb
+++ db/migrate/20220727153338_create_foods.rb
@@ -1,6 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
+      t.string :name
+      t.string :measurement_unit
+      t.integer :price
+      t.integer :quantity
+      t.belongs_to :user, index:true, foreign_key: true
 
       t.timestamps
     end
