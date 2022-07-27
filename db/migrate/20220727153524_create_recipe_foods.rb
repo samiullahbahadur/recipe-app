1d3ce03c2d23855feb54d173764aa2e3b291158e
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 20:22:57 +0430

    Add recipes_foo  migration

--- db/migrate/20220727153524_create_recipe_foods.rb
+++ db/migrate/20220727153524_create_recipe_foods.rb
@@ -1,7 +1,10 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
-
+      t.integer :quantity
+      t.belongs_to :recipe, index:true , foreign_key:true
+      t.belongs_to :food, index:true , foreign_key:true
+     
       t.timestamps
     end
   end
