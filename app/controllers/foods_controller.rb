945bad27e7b9f7b0223bcc327719851959931003
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 22:00:59 +0430

    Add create method in food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -6,4 +6,20 @@ class FoodsController < ApplicationController
     @food=Food.new
     @user = current_user
   end
+  def create 
+    @food =Food.new(food_params)
+    @food.user=current_user
+    if @food.save
+      flash[:notice]="New Food successfully Saved! "
+      redirect_to foods_path
+
+    else
+      flash[:alert]="somethings was wrong "
+      render 'new'
+    end
+  end
+  private
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
