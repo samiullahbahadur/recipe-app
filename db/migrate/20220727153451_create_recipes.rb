bf0c6750127a4e3bf65ddcfaed05ea0bd6343d31
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 20:22:14 +0430

    Add recipes  migration

--- db/migrate/20220727153451_create_recipes.rb
+++ db/migrate/20220727153451_create_recipes.rb
@@ -1,7 +1,12 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :preparation_time
+      t.string :cooking_time
+      t.string :desciption
+      t.boolean :public
+      t.belongs_to  :user,  index:true, foreign_key:true
       t.timestamps
     end
   end
