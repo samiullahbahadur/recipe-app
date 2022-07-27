d2dbe14bb9414f68f1d2aa89da706b218ccc0454
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 23:38:45 +0430

    Add index method in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   def index
+    @recipe=Recipe.all
   end
 
   def new
@@ -15,11 +16,11 @@ class RecipesController < ApplicationController
     redirect_to recepes_path
   end
 end
+
 private
+
  def recipe_params 
   params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :desciption, :public)
-
  end
-
 
 end
