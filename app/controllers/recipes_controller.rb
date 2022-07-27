8a54a90526a55e1398f25b77bdd1b6585df41157
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-27 23:33:13 +0430

    Add create method in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,12 @@ class RecipesController < ApplicationController
     @recipe=Recipe.new
     @user=current_user
   end
+
+  def create 
+    @recipe=Recipe.new(recipe_params)
+    @recipe.user=current_user
+    fi @recipe.save
+    flash[:notice]="New recipe successfully Saved! "
+    redirect_to recepes_path
+  end
 end
