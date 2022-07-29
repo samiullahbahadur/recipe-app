f80af6917c8da6f924c318352c2a07b52cf7c769
samiullahbhadur <bahadursamiullah@gmail.com>
2022-07-29 16:34:12 +0430

    Update recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,37 +1,42 @@
 class RecipesController < ApplicationController
   def index
-    @recipe = Recipe.all
-  end
-
-  def new
-    @recipe = Recipe.new
-    @user = current_user
-  end
-
-  def create
-    @recipe = Recipe.new(recipe_params)
-    @recipe.user = current_user
-    if @recipe.save
-      flash[:notice] = "New recipe successfully Saved! "
-      redirect_to recipes_path
-    end
+    @user = User.includes(:recipe).find(params[:user_id])
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
-    @food = Food.all
+    @user = User.includes(:recipe).find(params[:user_id])
+    @recipe = @user.recipe.includes(:recipe_foods).find(params[:id])
+    @current_user = current_user
+  end
+
+  def new
+    @user = current_user
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipe.new(recipe_params)
+    @recipe.user_id = params[:user_id]
+    respond_to do |format|
+      if @recipe.save!
+        format.html do
+          redirect_to user_recipe_path(@recipe.user_id, @recipe.id), notice: 'Recipe created successfully!'
+        end
+      else
+        format.html { redirect_to new_user_recipes_path, alert: 'Failed to create recipe' }
+      end
+    end
   end
 
   def destroy
-    @recipe = Recipe.find(params[:id])
-    @recipe.destroy
-    flash[:notice] = "recipe was successfully deleted"
-    redirect_to recipes_path
+    recipe = Recipe.find(params[:id])
+    recipe.destroy!
+    redirect_to user_recipes_path, notice: 'Recipe deleted successfully!'
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :desciption, :public)
+    params.require(:recipe).permit(:name, :desciption, :preparation_time, :cooking_time, :public)
   end
 end
